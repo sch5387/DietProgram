5197200eb92899b29162766b11e3b25e1192dc6f
Jordan Wayman <jaw6891@rit.edu>
2010-10-24 18:36:37 -0400

    Made some edits to the files working more on basis for future of what will be needed.

--- src/LogFile.cpp
+++ src/LogFile.cpp
@@ -6,10 +6,32 @@
 */
 
 using namespace std;
+#include <vector>
 
 class LogFile {
 
+private:
+	vector<string> logItems;
 
+public:
+	LogFile::LogFile(vector<string> logFileItems) {
+		logItems = logFileItems;
+	}
 
+	void LogFile::addItem() {
+
+	}
+
+	void LogFile::removeItem() {
+
+	}
+
+	void LogFile::undoChange() {
+
+	}
+
+	void LogFile::redoChange() {
+
+	}
 
 };
--- src/LogWriter.cpp
+++ src/LogWriter.cpp
@@ -1,10 +1,12 @@
 /*
 * LogWriter.cpp
+* <<IOStream>>
 *
 * Created October 24th, 2010
 * Author: jaw6891@rit.edu "Jordan Wayman"
 */
 
+#include <iostream>
 using namespace std;
 
 class LogWriter {
