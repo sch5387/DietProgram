5f22b530972ce58233ad84dd5cb719455ed6ff29
Rick <rxj9246@rit.edu>
2010-10-25 19:03:56 -0400

    Updated AddToLog and LogCommand.

--- src/AddToLog.cpp
+++ src/AddToLog.cpp
@@ -3,16 +3,28 @@
 *
 * Created October 24th, 2010
 * Author: jaw6891@rit.edu "Jordan Wayman"
+* Author: rxj9246@g.rit.edu  Richard Jester
 */
+
+#include "LogCommand.h"
 
 using namespace std;
 
 class AddToLog: public LogCommand {
 
-	void execute() {
-		
-		
-	}
+	AddToLog::AddToLog()
+		LogCommand::LogCommand();
+	{}
+
+	void LogCommand::execute(vector*<String> v) {
+		cout("Please specify what food to add: ");
+		Sting food = cin();
+		cout("/nPlease specify the number of servings: ");
+		String servings = cin();
+		cout(/n"Please specify the date(mm/dd/yyyy): ");
+		String date = cin();
+		&v.push_back(date + ": " + servings + " servings of " + food);
+	};
 
 
 };
